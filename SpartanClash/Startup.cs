fac6c64153549463772a6d5c0580e220741de73d
Connor Kennelly <knelly26@gmail.com>
2017-08-30 08:21:01 -0700

    Update routing.

--- SpartanClash/Startup.cs
+++ SpartanClash/Startup.cs
@@ -61,7 +61,7 @@ namespace SpartanClash
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{company?}");
             });
         }
     }
