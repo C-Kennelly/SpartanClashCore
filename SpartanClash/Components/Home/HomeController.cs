a49c70d72eca687eb967a2d2e632f5df6f9db6bf
Connor Kennelly <knelly26@gmail.com>
2017-12-04 13:02:37 -0800

    Refactor company autocomplete and try some basic CSS.

--- SpartanClash/Components/Home/HomeController.cs
+++ SpartanClash/Components/Home/HomeController.cs
@@ -4,27 +4,47 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SpartanClash.Data;
+using SpartanClash.Models;
 using SharedViews.Models;
 
 namespace Home
 {
     public class HomeController : Controller
     {
+        clashdbContext _clashdbContext;
+
+        public HomeController(clashdbContext context)
+        {
+            _clashdbContext = context;
+        }
+
         public IActionResult Index()
         {
             //return RedirectToAction(nameof(DailyLoop.DailyLoopController.Index), "DailyLoop");
             return View();
         }
 
-        public IActionResult Autocomplete(string term) 
+        public IActionResult CompanyAutocomplete(string term) 
         {
-            var items = new[] {"Apple", "Pear", "Banana", "Pineapple", "Peach"};
+            //var items = new[] {"Apple", "Pear", "Banana", "Pineapple", "Peach"};
+            //
+            //var filteredItems = items.Where(
+            //    item => item.IndexOf(term,
+            //    StringComparison.InvariantCultureIgnoreCase) >= 0
+            //);
 
-            var filteredItems = items.Where(
-                item => item.IndexOf(term, 
-                StringComparison.InvariantCultureIgnoreCase) >= 0
-            );
-            return Json(filteredItems);
+            using (var db = _clashdbContext)
+            {
+                var companies = db.TCompanies.ToArray();
+
+                var filteredCompanies = companies.Where(
+                    company => company.Company.IndexOf(term, 
+                    StringComparison.InvariantCultureIgnoreCase) >= 0
+                );
+
+                return Json(filteredCompanies);
+            }
         }
 
         public IActionResult About()
--- SpartanClash/Startup.cs
+++ SpartanClash/Startup.cs
@@ -112,13 +112,17 @@ namespace SpartanClash
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}");
+
+                routes.MapRoute(
                     name: "areaRoute",
                     template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}");
+
             });
+
+
         }
     }
 }
