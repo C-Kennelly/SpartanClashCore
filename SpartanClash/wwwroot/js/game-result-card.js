f017b7990a144e2abfbc5858b7a90c0e9eb9b620
Connor Kennelly <knelly26@gmail.com>
2018-01-07 15:13:37 -0800

    Add display filtering logic to Opponent button.

--- SpartanClash/wwwroot/js/game-result-card.js
+++ SpartanClash/wwwroot/js/game-result-card.js
@@ -18,3 +18,94 @@ window.onclick = function (event) {
         }
     }
 }
+
+/******  Filtering - Common  ******/
+
+function RecalculateAggregateStats() {
+
+}
+
+function showCardsWithCSSFilter(cardsToShow, cssFilterClass) {
+    for (var i = 0; i < cardsToShow.length; i++) {
+        cardsToShow[i].classList.remove(cssFilterClass);
+    }
+}
+
+function hideCardsWithCSSFilter(cardsToHide, cssFilterClass) {
+    for (var i = 0; i < cardsToHide.length; i++) {
+        cardsToHide[i].classList.add(cssFilterClass);
+    }
+}
+
+function ChangeButtonTextByID(button, newText) {
+    button.textContent = newText;
+}
+
+
+/******  Filtering - Opponent  ******/
+
+
+function showAllOpponentsUsing(cssFilterClass, textContent) {
+    var clanMatches = GetClanMatches();
+    var nonClanMatches = GetNonClanMatches();
+
+    showCardsWithCSSFilter(clanMatches, cssFilterClass);
+    showCardsWithCSSFilter(nonClanMatches, cssFilterClass);
+
+    OpponentCleanup(textContent);
+}
+
+function showOnlyClanOpponentsUsing(cssFilterClass, textContent) {
+    var clanMatches = GetClanMatches();
+    var nonClanMatches = GetNonClanMatches();
+
+    showCardsWithCSSFilter(clanMatches, cssFilterClass);
+    hideCardsWithCSSFilter(nonClanMatches, cssFilterClass);
+
+    OpponentCleanup(textContent);
+}
+
+function showOnlyNonClanOpponentsUsing(cssFilterClass, textContent) {
+    var clanMatches = GetClanMatches();
+    var nonClanMatches = GetNonClanMatches();
+
+    showCardsWithCSSFilter(nonClanMatches, cssFilterClass);
+    hideCardsWithCSSFilter(clanMatches, cssFilterClass);
+
+    OpponentCleanup(textContent);
+}
+
+    function OpponentCleanup(textToReplace) {
+        var opponentButton = document.getElementById("opponent-filter-btn");
+        ChangeButtonTextByID(opponentButton, textToReplace);
+    
+        RecalculateAggregateStats();
+    }
+       
+
+    function GetClanMatches() {
+        var clanMatchHeaderElements = document.getElementsByClassName('enemy-clan-link');
+        return GetParentGameResultCards(clanMatchHeaderElements);
+    }
+    
+    function GetNonClanMatches() {
+        var nonClanHeaderElements = document.getElementsByClassName('non-clan-header');
+        return GetParentGameResultCards(nonClanHeaderElements);
+    }
+    
+        function GetParentGameResultCards(childNodeList) {
+        
+            var matchingGameResultCards = [childNodeList.length];
+        
+            for (var i = 0; i < childNodeList.length; i++) {
+                matchingGameResultCards[i] = childNodeList[i].parentElement.parentElement;
+            }
+        
+            return matchingGameResultCards;
+        }
+
+/******  Filtering - Date  ******/
+
+
+
+/******  Filtering - Mode  ******/
