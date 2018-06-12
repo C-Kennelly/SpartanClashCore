f55bedf3c21b7e13f6a9109349d46c78a2c3c18d
Connor <knelly26@gmail.com>
2018-06-11 19:53:24 -0700

    Add base Registry page.

---
+++ SpartanClash/Components/Registry/RegistryController.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using SpartanClash.Models.ClashDB;
+
+namespace ServiceRecord
+{
+    public class RegistryController : Controller
+    {
+        clashdbContext _clashdbContext;
+
+        public RegistryController(clashdbContext context)
+        {
+            _clashdbContext = context;
+        }
+
+        public ActionResult All()
+        {
+            List<TCompanies> companyList = _clashdbContext.TCompanies.ToList();
+            return View(companyList);
+        }
+    }
+}
